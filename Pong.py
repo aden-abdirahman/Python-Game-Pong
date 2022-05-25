42a0e11080ff1a15ecae7afc1aff4563eb2b5d4b
aden-abdirahman <aden.abdirahman45@gmail.com>
2022-05-24 21:39:21 -0400

    made movement mechanics for paddles

--- Pong.py
+++ Pong.py
@@ -42,6 +42,57 @@ paddle_b.goto(+350, 0)
 
 
 # Ball
+ball = turtle.Turtle()
+
+ball.speed(0)
+
+ball.shape("circle")
+
+ball.color("white")
+
+ball.penup()
+
+ball.goto(0, 0)
+
+# Functions
+
+#defining the paddles upward movement 
+def paddle_a_up():
+    # ycor is the y coordinate of the turtle object. ycor is a property of the turtle object
+    y = paddle_a.ycor()
+    # adding 20px to yc coordinate
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    # subtracting 20px to yc coordinate
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    # subtracting 20px to yc coordinate
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    # subtracting 20px to yc coordinate
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+#built in methods in turtle for key events
+wn.listen()
+# when user presses "w" call function paddle_a_up
+wn.onkeypress(paddle_a_up, "w")
+# when user presses "s" call function paddle_a_down
+wn.onkeypress(paddle_a_down, "s")
+    
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+    
 
 #main game loop
 while True:
